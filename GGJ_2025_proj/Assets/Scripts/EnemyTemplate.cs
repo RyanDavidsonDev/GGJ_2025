48929e6fc10b5a35fa08f0f9afaa2234d2f938fe
SageLabesky <sage.labesky@du.edu>
2025-01-24 22:03:31 -0700

    Added Turret, Changed enemy default
    Added a die and take damage method to the enemy template

--- GGJ_2025_proj/Assets/Scripts/EnemyTemplate.cs
+++ GGJ_2025_proj/Assets/Scripts/EnemyTemplate.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyTemplate : MonoBehaviour
+public class EnemyTemplate : MonoBehaviour, IDamagable
 {
     [Header("Enemy Stats")]
     [SerializeField] private int health;
@@ -21,5 +21,21 @@ public class EnemyTemplate : MonoBehaviour
     void Update()
     {
         this.gameObject.transform.LookAt(Player);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        if(health <= 0){
+            Die();
+        }
+    }
+
+    public void Die(){
+        // Drop bubbles
+        for(int i = 0; i < bubblesDropped; i++){
+            // Drop a bubble
+        }
+        Destroy(this.gameObject);
     }
 }
