181897b49337d2d31b5b0db3c40a4ef5201964c9
SageLabesky <sage.labesky@du.edu>
2025-01-25 22:13:36 -0700

    Enemy Spawning Complete

--- GGJ_2025_proj/Assets/Scripts/EnemyScripts/TurretSpawner.cs
+++ GGJ_2025_proj/Assets/Scripts/EnemyScripts/TurretSpawner.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class TurretSpawner : MonoBehaviour
 {   
     [Header("Spawner Components")]
-    [SerializeField] private GameObject enemyPrefab;
-    [SerializeField] private Transform[] spawnPoints;
+    [SerializeField] private GameObject[] enemyPrefab;
+    [SerializeField] private GameObject[] spawnPoints;
     [SerializeField] private float spawnRate;
     [SerializeField] private int enemiesToSpawn;
     private Vector3[] randomPositions;
@@ -22,10 +22,21 @@ public class TurretSpawner : MonoBehaviour
 
             
             // Spawn an enemy
+            GameObject tempEnemy = enemyPrefab[Random.Range(0, enemyPrefab.Length)];
 
-            GameObject enemy = Instantiate(enemyPrefab, randomPositions[i] , enemyPrefab.transform.rotation);
-            //enemy.GetComponent<EnemyTemplate>().Player = GameObject.FindGameObjectWithTag("Player").transform;
-            enemy.GetComponent<EnemyTurret>().target = GameObject.FindGameObjectWithTag("Player").transform;
+            GameObject enemy = Instantiate(tempEnemy, randomPositions[i] , tempEnemy.transform.rotation);
+            
+            Debug.Log("enemy type: " + tempEnemy.name);
+            if(tempEnemy.name == "Turret"){
+                enemy.GetComponent<EnemyTurret>().target = GameObject.FindGameObjectWithTag("Player").transform;
+            } else if(tempEnemy.name == "Follower"){
+                enemy.GetComponent<EnemyFollower>().target = GameObject.FindGameObjectWithTag("Player").transform;
+            } else if(tempEnemy.name == "Mimic"){
+                enemy.GetComponent<EnemyMimic>().target = GameObject.FindGameObjectWithTag("Player").transform;
+            } else{
+                Debug.Log("No target found");
+            }
+            
             yield return new WaitForSeconds(spawnRate);
         }
     }
@@ -40,16 +51,25 @@ public class TurretSpawner : MonoBehaviour
 
         for (int i = 0; i < randomPositions.Length-1; i++)
         {
-            int randomSide = Random.Range(0, 4);
-        Debug.Log("random side is " + randomSide);
+            //int randomSide = Random.Range(0, 4);
+            //Debug.Log("random side is " + randomSide);
 
-            point1 = spawnPoints[randomSide].position;
-            point2 = spawnPoints[(randomSide + 1) % 4].position;//could change this to randPos[].len if we need more corners
+            //point1 = spawnPoints[randomSide].position;
+            //point2 = spawnPoints[(randomSide + 1) % 4].position;//could change this to randPos[].len if we need more corners
 
+            point1 = spawnPoints[0].transform.position;
+            point2 = spawnPoints[2].transform.position;
 
-            Debug.Log(" points are " + point1.ToString() + " and " + point2.ToString());
+            float newX = Random.Range(point1.x, point2.x);
+            float newZ = Random.Range(point1.z, point2.z);
 
-            randomPositions[i] = Vector3.Lerp(point1, point2, Random.Range(0.0f, 1.0f));
+            Vector3 newPoint = new Vector3(newX, 0, newZ);
+
+            Debug.Log("new point is " + newPoint.ToString());
+            //Debug.Log(" points are " + point1.ToString() + " and " + point2.ToString());
+
+            //randomPositions[i] = Vector3.Lerp(point1, point2, Random.Range(0.0f, 1.0f));
+            randomPositions[i] = newPoint;
         }
         return randomPositions;
     }
