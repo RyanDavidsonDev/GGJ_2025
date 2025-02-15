6e72c9aa60fae5523b1c75e5922a248d283a6cf4
Liam <Liam.King@du.edu>
2025-02-15 12:12:32 -0700

    xp orbs despawn

--- GGJ_2025_proj/Assets/Scripts/XP_Progression/XP_Orb.cs
+++ GGJ_2025_proj/Assets/Scripts/XP_Progression/XP_Orb.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class XP_Orb : MonoBehaviour
 {
     [SerializeField] private Collider orbCollider;
+
     [Tooltip("This dictates a range for the amount of XP to drop, X is min, Y is max")]
     [SerializeField] private Vector2 XPAmount;
+
     [Tooltip("The max distance the orb will move on creation")]
     [SerializeField] private float moveSpeedRange;
     [SerializeField] private float moveDecaySpeed;
+
+    [Tooltip("The amount of time in seconds the orb will stay in the scene before it gets disabled")]
+    [SerializeField] private int despawnTime = 10;
 
     private Vector2 direction;
     private float moveSpeed;
@@ -19,13 +25,18 @@ public class XP_Orb : MonoBehaviour
 
     private void Start()
     {
-        orbCollider = GetComponent<Collider>();
+        //orbCollider = GetComponent<Collider>();
+
         float floatAngle = Random.Range(0f, 360f);
         direction = new Vector2 (Mathf.Sin(floatAngle * Mathf.Deg2Rad), Mathf.Cos(floatAngle * Mathf.Deg2Rad));
         moveSpeed = Random.Range(0.1f, moveSpeedRange);
         exp = Mathf.RoundToInt(Random.Range(XPAmount.x, XPAmount.y));
         transform.localScale = Vector3.one * (exp / XPAmount.y);
+
+        IEnumerator desCot = DespawnCoroutine(despawnTime);
+
         StartCoroutine(Move());
+        StartCoroutine(desCot);
     }
 
     public IEnumerator Move()
@@ -38,6 +49,18 @@ public class XP_Orb : MonoBehaviour
             yield return null;
         }
     }
+
+    public IEnumerator DespawnCoroutine(int despawnTime)
+    {
+        while (despawnTime > 0)
+        {
+            despawnTime--;
+            yield return new WaitForSeconds(1f);
+        }
+
+        Destroy(gameObject);
+    }
+
     public int GetXP()
     {
         SFXManager.Instance.PlaySound(SFXManager.Instance.BubbleCollect);
